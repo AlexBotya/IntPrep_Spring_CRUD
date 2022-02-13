bc7cdc3992b5c629562670a5286b02804c5f6f5d
AlexBotya <alex_balg@bk.ru>
2022-02-13 20:07:31 +0300

    Initial commit

---
+++ src/main/java/ru/gb/intprep_spring_crud/IntPrepSpringCrudApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.intprep_spring_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntPrepSpringCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntPrepSpringCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gb/intprep_spring_crud/IntPrepSpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.intprep_spring_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntPrepSpringCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
